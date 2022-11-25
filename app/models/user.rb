bcce8c47e9792dee713dcfe9b0e432840c993a59
mleclerc182 <mleclerc182@gmail.com>
2022-11-25 18:37:40 -0500

    Devise Gem Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 8..128
+
+  validate :password_complexity
+
+
+  def password_complexity
+    if password.present?
+      unless password.match(/^(?=.*[a-z])(?=.*[A-Z])/)
+        errors.add :password, " complexity requirement not met"
+      end
+    end
+  end
+
 end
+
