522f0fa3b7878f5ec50239b7438ab6bb11cf7263
mleclerc182 <mleclerc182@gmail.com>
2022-12-04 23:47:34 -0500

    Profile stuff

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,33 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+
+  def create
+    #@user = User.find(params[:user_id])
+    # @profile = @user.build_profile(profile_params)
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if Profile.exists? user_id: current_user.id
+      update
+    else
+      if @profile.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  end
+
+  def update
+    @profile = Profile.find_by(user_id: current_user.id)
+    if @profile.update(profile_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
 end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
 belongs_to :user
+validates :first_name, presence: true, allow_blank: false
+validates :last_name, presence: true, allow_blank: false
+validates :job_title, presence: true, allow_blank: false
+validates :phone_number, presence: true, allow_blank: false
+validates :contact_email, presence: true, allow_blank: false
+validates :description, presence: true, allow_blank: false
+
+
 end
